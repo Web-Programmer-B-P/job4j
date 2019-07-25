253a61b9174e054a619c94739ec7c57188923ea9
Web-Programmer-B-P <barsukov2805@gmail.com>
2019-07-25 10:08:30 +0300

    4.3. Вычисление площади треугольника[#159698]

--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -1,4 +1,14 @@
 package ru.job4j.condition;
 
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class PointTest {
+    @Test
+    public void checkPoint(){
+        Point check = new Point();
+        double result = check.distance(3, -5, 2, 9);
+        assertThat(result, is(14.035668847618199));
+    }
 }
