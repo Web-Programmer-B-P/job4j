04226e4864d08ad31beddadd637b5431a384c549
Web-Programmer-B-P <barsukov2805@gmail.com>
2019-08-06 11:03:01 +0300

    2. Рефакторинг - Площадь треугольника.[#159824]

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -14,8 +14,8 @@ public class Triangle {
      * @param c расстояние между точками b c
      * @return полуперимента.
      */
-    public double period(Point a, Point b, Point c) {
-        return (a.distance(b) + b.distance(c) + c.distance(a)) / 2;
+    public double period(double a, double b, double c) {
+        return (a + b + c) / 2;
     }
 
     /**
@@ -24,14 +24,12 @@ public class Triangle {
      * Подумайте какое надо написать условие, чтобы определить можно ли построить треугольник.
      *
      * @param a Длина от точки a b.
-     * @param c Длина от точки b c.
      * @param b Длина от точки a c.
+     * @param c Длина от точки b c.
      * @return
      */
-    private boolean exist(Point a, Point c, Point b) {
-        if (a.distance(b) < (a.distance(c) + c.distance(b))
-                && a.distance(c) < (a.distance(b) + c.distance(b))
-                && c.distance(b) < (a.distance(b) + a.distance(c))) {
+    private boolean exist(double a, double b, double c) {
+        if (a < (b + c) && b < (a + c) && c < (b + a)) {
             return true;
         }
         return false;
@@ -50,16 +48,16 @@ public class Triangle {
      */
     public double area(int x1, int y1, int x2, int y2, int x3, int y3) {
         double rsl = -1;
-        Point a = new Point(x1, y1);
-        Point b = new Point(x2, y2);
-        Point c = new Point(x3, y3);
-        a.distance(b);
-        b.distance(c);
-        c.distance(a);
+        Point first = new Point(x1, y1);
+        Point second = new Point(x2, y2);
+        Point third = new Point(x3, y3);
+        double a = first.distance(second);
+        double b = first.distance(third);
+        double c = second.distance(third);
         double p = period(a, b, c);
         if (this.exist(a, b, c)) {
             // написать формулу для расчета площади треугольника.
-            rsl = Math.sqrt(p * (p - a.distance(b)) * (p - a.distance(c)) * (p - c.distance(b)));
+            rsl = Math.sqrt(p * (p - a) * (p - b) * (p - c));
         }
         return rsl;
     }
