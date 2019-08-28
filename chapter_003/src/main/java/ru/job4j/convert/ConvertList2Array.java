46491fb1d0256925df75795f54bdb05a10c3356d
Web-Programmer-B-P <barsukov2805@gmail.com>
2019-08-28 18:48:07 +0300

    3. Конвертация ArrayList в двухмерный массив[#159782]

--- chapter_003/src/main/java/ru/job4j/convert/ConvertList2Array.java
+++ chapter_003/src/main/java/ru/job4j/convert/ConvertList2Array.java
@@ -1,23 +1,20 @@
 package ru.job4j.convert;
 
-import java.util.Iterator;
 import java.util.List;
 
 public class ConvertList2Array {
     public int[][] toArray(List<Integer> list, int rows) {
-        int cells = rows;
+        int cells = list.size() % rows == 0 ? list.size() / rows : (list.size() / rows) + 1;
         int[][] array = new int[rows][cells];
-        int count = 0;
-        for (int[] out : array) {
-            int index = 0;
-            for (int inner : out) {
-                if (count <= list.size() - 1) {
-                    out[index] = list.get(count++);
-                } else {
-                    out[index] = 0;
-                }
-                index++;
+        int outer = 0;
+        int inner = 0;
+        for (Integer el : list) {
+            if (inner == cells) {
+                outer++;
+                inner = 0;
             }
+            array[outer][inner] = el;
+            inner++;
         }
         return array;
     }
--- chapter_003/src/test/java/ru/job4j/convert/ConvertList2ArrayTest.java
+++ chapter_003/src/test/java/ru/job4j/convert/ConvertList2ArrayTest.java
@@ -12,14 +12,13 @@ public class ConvertList2ArrayTest {
     public void when7ElementsThen9() {
         ConvertList2Array list = new ConvertList2Array();
         int[][] result = list.toArray(
-                Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13),
-                4
+                Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
+                3
         );
         int[][] expect = {
                 {1, 2, 3, 4},
                 {5, 6, 7, 8},
-                {9, 10, 11, 12},
-                {13, 0, 0, 0}
+                {9, 10, 0, 0}
         };
         assertThat(result, is(expect));
     }
