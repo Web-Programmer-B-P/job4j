fc09e02b58656278a33868bc64fe455d7caf2385
Web-Programmer-B-P <barsukov2805@gmail.com>
2019-09-02 10:45:21 +0300

    2. Сортировка User с использованием Comparator[#159772]

--- chapter_003/src/main/java/ru/job4j/sort/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/sort/SortUser.java
@@ -2,9 +2,7 @@ package ru.job4j.sort;
 
 import ru.job4j.sort.model.User;
 
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 /**
  * Class SortUser
@@ -13,9 +11,49 @@ import java.util.TreeSet;
  * @since 31.08.2019
  */
 public class SortUser {
+    /**
+     * Метод сортировки пользователей по колличеству лет.
+     * @param users
+     * @return
+     */
     public Set<User> sort(List<User> users) {
         Set<User> sortList = new TreeSet<>();
         sortList.addAll(users);
         return sortList;
+    }
+
+    /**
+     * Метод сортировки пользователей по длине имени.
+     * @param users
+     * @return
+     */
+    public List<User> sortNameLeght(List<User> users) {
+        users.sort(
+                new Comparator<User>() {
+                    @Override
+                    public int compare(User o1, User o2) {
+                        return o1.getName().compareTo(o2.getName());
+                    }
+                }
+        );
+        return users;
+    }
+
+    /**
+     * Метод сортирует по всем полям
+     * @param users
+     * @return
+     */
+    public List<User> sortByAllFields(List<User> users) {
+        users.sort(
+                new Comparator<User>() {
+                    @Override
+                    public int compare(User o1, User o2) {
+                        int equals = o1.getName().compareTo(o2.getName());
+                        return equals == 0 ? (o1.getName() + o1.getAge()).compareTo(o2.getName() + o2.getAge()) : equals;
+                    }
+                }
+        );
+        return users;
     }
 }
