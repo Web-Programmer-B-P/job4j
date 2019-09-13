752d616c7605b00e1f3af15d80399114a93f4a2f
Web-Programmer-B-P <barsukov2805@gmail.com>
2019-09-13 21:19:03 +0300

    1. Collection API Улучшения[#159676]

--- chapter_003/src/test/java/ru/job4j/student/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/student/SortUserTest.java
@@ -12,31 +12,52 @@ public class SortUserTest {
     @Test
     public void whenSortAndReturnListUserHowTreeSet() {
         SortUser input = new SortUser();
-        Set<User> result = input.sort(Arrays.asList(new User("Izmail", 48),
-                new User("Turambek", 35), new User("Lenya", 21)));
-        String excpected = "[User{name='Lenya', age=21}, User{name='Turambek', age=35}, User{name='Izmail', age=48}]";
-        assertThat(result.toString(), is(excpected));
+        Set<User> result = input.sort(
+                Arrays.asList(
+                        new User("Turambek", 35),
+                        new User("Lenya", 21),
+                        new User("Izmail", 48)
+                )
+        );
+        List<User> excpected = List.of(
+                new User("Izmail", 48),
+                new User("Lenya", 21),
+                new User("Turambek", 35)
+                );
+        assertThat(result.toString(), is(input.sort(excpected).toString()));
     }
 
     @Test
     public void whenSortUserByNameLeght() {
         SortUser call = new SortUser();
-        List<User> input = Arrays.asList(new User("Сергей", 25), new User("Иван", 30),
-                new User("Сергей", 20), new User("Иван", 25));
+        List<User> input = Arrays.asList(
+                new User("Сергей", 25), new User("Иван", 30),
+                new User("Сергей", 20), new User("Иван", 25)
+                );
         List<User> result = call.sortNameLeght(input);
-        String excpected = "[User{name='Иван', age=30}, User{name='Иван', age=25}, User{name='Сергей', age=25}, "
-                + "User{name='Сергей', age=20}]";
-        assertThat(result.toString(), is(excpected));
+        List<User> excpected = List.of(
+                new User("Иван", 30),
+                new User("Иван", 25),
+                new User("Сергей", 25),
+                new User("Сергей", 20)
+        );
+        assertThat(result.toString(), is(excpected.toString()));
     }
 
     @Test
     public void whenSortByBothFieldsOfUser() {
         SortUser call = new SortUser();
-        List<User> input = Arrays.asList(new User("Сергей", 25), new User("Иван", 30),
-                new User("Сергей", 20), new User("Иван", 25));
+        List<User> input = Arrays.asList(
+                new User("Сергей", 25), new User("Иван", 30),
+                new User("Сергей", 20), new User("Иван", 25)
+        );
         List<User> result = call.sortByAllFields(input);
-        String excpected = "[User{name='Иван', age=25}, User{name='Иван', age=30}, User{name='Сергей', age=20}, "
-                + "User{name='Сергей', age=25}]";
-        assertThat(result.toString(), is(excpected));
+        List<User> excpected = List.of(
+                new User("Иван", 25),
+                new User("Иван", 30),
+                new User("Сергей", 20),
+                new User("Сергей", 25)
+        );
+        assertThat(result.toString(), is(excpected.toString()));
     }
 }
