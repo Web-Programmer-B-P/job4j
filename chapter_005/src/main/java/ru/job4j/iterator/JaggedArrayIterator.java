79da71a347514ced309946653a6343a90264ce8a
Web-Programmer-B-P <barsukov2805@gmail.com>
2019-10-01 16:25:28 +0300

    5.1.1. Итератор для двухмерного массива int[][][#179862]

---
+++ chapter_005/src/main/java/ru/job4j/iterator/JaggedArrayIterator.java
@@ -0,0 +1,53 @@
+package ru.job4j.iterator;
+
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Class IteratorArray
+ *
+ * @author Petr B.
+ * @since 01.10.2019, 9:42
+ */
+public class JaggedArrayIterator implements Iterator {
+    private final int[][] array;
+    private int outer;
+    private int inner;
+    private int count;
+    private int size;
+
+    public JaggedArrayIterator(final int[][] array) {
+        this.array = array;
+    }
+
+    @Override
+    public boolean hasNext() {
+        boolean result = false;
+        if (this.array.length > this.outer + 1 || this.inner < this.count) {
+            result = true;
+        }
+        return result;
+    }
+
+    @Override
+    public Object next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException("Sorry, you can`t use this method with empty array!");
+        } else if (this.outer >= this.size && this.outer <= this.array.length - 1) {
+            this.count = 0;
+            for (int value : this.array[this.outer]) {
+                this.count++;
+            }
+        }
+
+        if (this.inner < this.count) {
+            this.size++;
+        } else {
+            this.size = this.outer;
+            this.inner = 0;
+            this.outer++;
+        }
+        return this.array[this.outer][this.inner++];
+    }
+}
