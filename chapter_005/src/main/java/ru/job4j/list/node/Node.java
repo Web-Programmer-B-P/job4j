6c1cae13066bf70c1f3d957cc57c2adf615bb7d3
Web-Programmer-B-P <barsukov2805@gmail.com>
2019-10-12 09:43:45 +0300

    5.3.4. Задан связанный список. Определить цикличность.[#179845]

---
+++ chapter_005/src/main/java/ru/job4j/list/node/Node.java
@@ -0,0 +1,18 @@
+package ru.job4j.list.node;
+
+import java.util.Objects;
+
+/**
+ * Class Node
+ *
+ * @author Petr B.
+ * @since 11.10.2019, 19:20
+ */
+public class Node<T> {
+    public T value;
+    public Node<T> next;
+
+    public Node(T value) {
+        this.value = value;
+    }
+}
