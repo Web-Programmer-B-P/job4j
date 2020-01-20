b433c823453d179fcce637978ca797925a5eb822
Web-Programmer-B-P <barsukov2805@gmail.com>
2020-01-20 10:15:10 +0300

    2. ExecutorService рассылка почты.[#214683]

---
+++ chapter_010/src/test/java/ru/job4j/email/EmailNotificationTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.email;
+
+import org.junit.Test;
+import ru.job4j.email.model.User;
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.*;
+
+/**
+ * Class EmailNotificationTest
+ *
+ * @author Petr B.
+ * @since 20.01.2020, 10:01
+ */
+public class EmailNotificationTest {
+    @Test
+    public void whenSendEmail() {
+        User user1 = new User("First User", "first@mail.ru");
+        User user2 = new User("Second User", "second@mail.ru");
+        EmailNotification poolEmailTasks = new EmailNotification();
+        poolEmailTasks.emailTo(user1);
+        poolEmailTasks.close();
+        assertThat(user1.getEmailFlag(), is(true));
+        assertThat(user2.getEmailFlag(), is(false));
+    }
+}
